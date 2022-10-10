4990ca2bb43e27cc9faba56e95e207001737481d
Farhan Dwi <82132098+minerva365@users.noreply.github.com>
2022-10-10 08:44:26 -0700

    Update libjsp.sql

--- libjsp.sql
+++ libjsp.sql
@@ -4,22 +4,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `lib`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lib`
---
-
 CREATE TABLE `lib` (
   `id` int(15) NOT NULL,
   `img` varchar(250) NOT NULL,
